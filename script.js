d1119ae44ff12ca61105e994135c1214e0669bb9
abdulwahid <abdulwahid126@gmail.com>
2024-12-26 00:54:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+//  Hero Section Code
+function toggleDisplay() {
+    // Get the element you want to toggle
+    const heroSection = document.getElementById("hero_container");
+    const formSection = document.getElementById("form-Section");
+    if (heroSection) {
+        // Toggle the display property
+        heroSection.style.display = "none";
+    }
+    if (formSection) {
+        formSection.style.display = "block";
+    }
+}
+// Add an event listener to the button
+const toggleButton = document.getElementById("buildBtn");
+if (toggleButton) {
+    toggleButton.addEventListener("click", toggleDisplay);
+}
+// Foam Section Code
